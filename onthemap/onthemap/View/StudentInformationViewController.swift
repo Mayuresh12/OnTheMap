e040834503f2447b1ec196be1e514efc57d45f0b
MRAO0212 <mayureshvrao@gmail.com>
2020-07-18 23:47:13 -0700

    add pin information controller

---
+++ onthemap/onthemap/View/StudentInformationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StudentInformationViewController.swift
+//  onthemap
+//
+//  Created by Mayuresh Rao on 7/18/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudentInformationViewController: UIViewController {
+ 
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
